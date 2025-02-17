40c72338112a4d01849b5b757f1e8facceec5fe1
Juan Tobar <juanpablococtm@gmail.com>
2025-02-16 20:18:57 -0500

    Agregar gestión de tareas con almacenamiento local y eventos de interacción

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
-let todo = 1
+const todoInput = document.getElementById("todoInput");
+
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
+
+// Initialize
+
+document.addEventListener("DOMContentLoaded", function () {
+  addButton.addEventListener("click", addTask());
+  todoInput.addEventListener("keydown", (event) => {
+    if (event) {
+      event.preventDefault();
+      addTask();
+    }
+  });
+  deleteButton.addEventListener("click", deleteAllTasks());
+  displayTasks();
+});
+
+const addTask = () => {};
+
+const deleteAllTasks = () => {};
